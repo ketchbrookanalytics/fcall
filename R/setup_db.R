74645ef221e115fff8262f6ecff470fea8b17249
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-02-14 16:14:21 -0500

    Created `setup_db.R`

---
+++ R/setup_db.R
@@ -0,0 +1,33 @@
+library(DBI)
+library(glue)
+
+con <- DBI::dbConnect(
+
+)
+
+glue::glue_sql(
+  "
+  CREATE TABLE inst (
+    SYSTEM int NOT NULL,
+    DIST int NOT NULL,
+    ASSOC int NOT NULL,
+    MONTH int NOT NULL,
+    YEAR int NOT NULL,
+    UNINUM int NOT NULL,
+    SHORTNAME varchar(50) NOT NULL,
+    MAIL_ADDR varchar(60) NOT NULL,
+    STREET_ADDR varchar(60) NOT NULL,
+    CITY varchar(30) NOT NULL,
+    STATE char(2) NOT NULL,
+    ZIP char(5) NOT NULL,
+  )
+  "
+)
+
+
+upload_INST <- function(data) {
+
+
+
+
+}
