9de5818667de62a48f5a3f541208e5c4e3feb3a2
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-02-14 16:15:41 -0500

    Converting col names to snake_case

--- R/setup_db.R
+++ R/setup_db.R
@@ -8,18 +8,18 @@ con <- DBI::dbConnect(
 glue::glue_sql(
   "
   CREATE TABLE inst (
-    SYSTEM int NOT NULL,
-    DIST int NOT NULL,
-    ASSOC int NOT NULL,
-    MONTH int NOT NULL,
-    YEAR int NOT NULL,
-    UNINUM int NOT NULL,
-    SHORTNAME varchar(50) NOT NULL,
-    MAIL_ADDR varchar(60) NOT NULL,
-    STREET_ADDR varchar(60) NOT NULL,
-    CITY varchar(30) NOT NULL,
-    STATE char(2) NOT NULL,
-    ZIP char(5) NOT NULL,
+    system int NOT NULL,
+    district int NOT NULL,
+    association int NOT NULL,
+    month int NOT NULL,
+    year int NOT NULL,
+    uninum int NOT NULL,
+    short_name varchar(50) NOT NULL,
+    mailing_address varchar(60) NOT NULL,
+    street_address varchar(60) NOT NULL,
+    city varchar(30) NOT NULL,
+    state char(2) NOT NULL,
+    zip char(5) NOT NULL,
   )
   "
 )
